1e4c44e0660dfa4fd19aaec0ec86a4bf38382c16
Fiza Husain <43874107+fiza11@users.noreply.github.com>
2019-05-07 12:51:53 +0530

    Update 1097A.cpp

--- Codeforces/1097A.cpp
+++ Codeforces/1097A.cpp
@@ -5,7 +5,7 @@ main(){
   char a[3],b[3],c[3],d[3],e[3],f[3];
   cin>>a;
   cin>>b>>c>>d>>e>>f;
- //just checking something
+ //wee
   if(b[0]==a[0] || c[0]==a[0] || d[0]==a[0] || e[0]==a[0] || f[0]==a[0] || b[1]==a[1] || c[1]==a[1] || d[1]==a[1] || e[1]==a[1] || f[1]==a[1] )
   cout<<"YES"<<endl;
   else cout<<"NO"<<endl;
